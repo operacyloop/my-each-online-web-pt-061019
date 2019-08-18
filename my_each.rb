5eee9ad5305fabd5b41cba5480c256382dec68fd
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-18 20:01:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,16 @@
-def my_each(argument) # put argument(s) here
-  i = 0 
-  while i < 4 
-    yield 
-    i += 1 
-  end
-  return argument
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
 
-my_each { puts "Now I'm inside the block!" }
+
+# def my_each(argument) # put argument(s) here
+#   i = 0 
+#   while i < 4 
+#     yield 
+#     i += 1 
+#   end
+#   return argument
+# end
+
+# my_each { puts "Now I'm inside the block!" }
